fe9d7f32bccb077c67f3828e3abcea76425c9479
Rgvs <gvsrahul@gmail.com>
2017-12-11 13:57:17 -0500

    basic graph builing from uber data is done partially till getting rdd for edges and rdds for vertex

---
+++ src/main/scala/GraphBuild.scala
@@ -0,0 +1,60 @@
+import org.apache.log4j.{Level, Logger}
+import org.apache.spark.{SparkConf, SparkContext}
+import java.io._
+import scala.util.parsing.json.JSON
+import scala.io._
+
+class SimpleCSVHeader(header:Array[String]) extends Serializable {
+  val index = header.zipWithIndex.toMap
+  def apply(array:Array[String], key:String):String = array(index(key))
+}
+
+object GraphBuild {
+  def main(args: Array[String]): Unit = {
+    Logger.getLogger("org").setLevel(Level.OFF)
+    Logger.getLogger("akka").setLevel(Level.OFF)
+
+    val conf = new SparkConf()
+    conf.setAppName("Uber")
+    conf.setMaster("local[2]")
+    val sc = new SparkContext(conf)
+
+    // reads hourly aggreagate
+    val csv = sc.textFile("./data/boston-censustracts-2016-1-All-HourlyAggregate.csv")
+    // spliting by comman and trimming elements
+    val data = csv.map(line => line.split(",").map(elem => elem.trim))
+    // building our header with the first line,
+    // it is used to get data from the element without using index values like ._1 for source id
+    val header = new SimpleCSVHeader(data.take(1)(0))
+    // filter the header out
+    val rows = data.filter(line => header(line, "dstid") != "dstid")
+    // getting only dstIds
+    val dstIds = rows.map(row => header(row,"dstid"))
+    // getting map from source ids to dst ids
+    val sourceToDst = rows.map(row => header(row,"sourceid").toInt -> header(row,"dstid").toInt)
+
+    println(dstIds.collect()(2))
+    println(sourceToDst.collect()(2))
+
+    // getting vertex data from boundaries file
+    val raw_movements = Source.fromFile("./data/boston_censustracts.json").getLines.mkString
+
+    // Json Parsing and getting features
+    val movements = JSON.parseFull(raw_movements)
+        .get.asInstanceOf[Map[String, Any]]("features")
+        .asInstanceOf[List[Map[String, Any]]]
+
+    // parallizing the vertex data and tranforming into a tuple of (movement_id, display_name, coordinates)
+    // display name is the street name,
+    // coordinates are of multi polygon
+    val movements_rdd = sc.parallelize(movements)
+      .map(feature => (feature("properties").asInstanceOf[Map[String, Any]]("MOVEMENT_ID").asInstanceOf[String].toInt,
+        feature("properties").asInstanceOf[Map[String, Any]]("DISPLAY_NAME").asInstanceOf[String],
+        feature("geometry").asInstanceOf[Map[String, Any]]("coordinates")
+          .asInstanceOf[List[List[List[List[Float]]]]]))
+
+
+    println(movements_rdd.collect()(0))
+    sc.stop()
+  }
+}
